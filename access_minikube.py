62683daa64a9d5390f841080d534de26cc4534a4
Samuel1P <ipsamuelip@gmail.com>
2020-08-05 23:01:07 +0530

    added doc string for two methods

--- access_minikube.py
+++ access_minikube.py
@@ -1,5 +1,5 @@
 from kubernetes import client, config as k8_config, watch
-from pdb import set_trace as db
+from pdb import set_trace as debug
 kube_config = "C:\Program Files\Kubernetes_Files\k8s_repo\k8s_python\KUBECONFIG"
 k8_config.load_kube_config(config_file=kube_config)
 v1 = client.CoreV1Api()
@@ -7,7 +7,10 @@ v1_apps = client.AppsV1Api()
 
 
 def get_pods_details_in_default_namespace():
-
+    """
+    This method fetches all the pod name, pod ip and pod status in the default namespace.
+    :return: None
+    """
     pod_list = v1.list_namespaced_pod("default")
     for pod in pod_list.items:
         print(f" Pod Name : {pod.metadata.name}, "
@@ -23,10 +26,12 @@ def get_pods_details_in_dynamic_namespace(ns):
               f"Pod IP : {pod.status.pod_ip}")
 
 def watch_pods_in_default_ns():
-    #stream = watch.Watch().stream(v1.list_namespaced_pod("default"))
+    """
+    This method watches the event type, pod name and pod status of the default namespace and print when there is a change.
+    :return: None
+    """
     stream = watch.Watch().stream(v1.list_namespaced_pod, "default")
     for event in stream:
-        #db()
         print (event['type'], event['object'].metadata.name, event['object'].status.phase)
 
 def get_deployments_in_a_ns(ns=''):
@@ -38,5 +43,6 @@ def get_deployments_in_a_ns(ns=''):
 if __name__ == "__main__":
     #get_pods_details_in_default_namespace()
     #get_pods_details_in_dynamic_namespace("kube-system")
-    watch_pods_in_default_ns()
+    #watch_pods_in_default_ns()
     #get_deployments_in_a_ns("")
+    pass
